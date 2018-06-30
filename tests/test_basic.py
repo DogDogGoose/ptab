f42daea031e092e21d728c04e62ace85b9c77fb9
Jeffrey Liang <jeffypop@gmx.com>
2018-06-30 10:42:38 -0700

    changed default output directory to the docket number

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -19,7 +19,7 @@ print ("Running basic test...")
 parser = argparse.ArgumentParser(description='Grab and properly name documents from PTAB dockets.')
 parser.add_argument('--verbose', '-v', type=bool, default=True, help='Wow much talk.', dest='verbose')
 parser.add_argument('--dkt', '-d', default="2016-01181", required=False, help='Docket Number')
-parser.add_argument('--outdir', '-o', required=False, default="../temp", help='Place to put output PDF files')
+parser.add_argument('--outdir', '-o', required=False, help='Place to put output PDF files')
 parser.add_argument('--dd', action='store_true', help="Don't download PDF files")
 parser.add_argument('--dump', action='store_true', help="Save JSON to file for debug")
 
@@ -28,6 +28,10 @@ args = parser.parse_args()
 myptab = ptab.core.ptabgrab(True)
 if args.dd: myptab.download = False
 if args.dump: myptab.dumpJson = True
+
+if not args.outdir:
+    args.outdir = "../temp/" + args.dkt
+
 myptab.setOutputDir(args.outdir)
 myptab.getDocsInDocket(args.dkt)
 
